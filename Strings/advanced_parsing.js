422f67d158c27c7e922b16e8e008abf525c1ff69
bp442 <bpark135@gmail.com>
2023-10-25 13:24:17 -0400

    added exercises up to 2-18

--- Strings/advanced_parsing.js
+++ Strings/advanced_parsing.js
@@ -4,10 +4,10 @@ function parseAndDisplayName(inputName){
    
     const nameArray = inputName.split(" ");
 
-    if (nameArray.length == 1){
+    if (nameArray.length == 1){ //only first name
         console.log("Name : " + inputName);
     }
-    else if(nameArray.length == 2){
+    else if(nameArray.length == 2){ //first and last
         let first = nameArray[0];
         let last = nameArray[1];
 
@@ -15,7 +15,7 @@ function parseAndDisplayName(inputName){
         console.log("First name: " + first);
         console.log("Last name : " + last);
     }
-    else if(nameArray.length == 3){
+    else if(nameArray.length == 3){ // first, middle, and last
         let first = nameArray[0];
         let middle = nameArray[1];
         let last = nameArray[2];
---
+++ Strings/replace_strings.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let message = "Our corporate offices are located in Dallas";
+
+let newMessage = message.replace(/dallas/i, "Austin");
+
+console.log(newMessage);
